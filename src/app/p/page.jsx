be9f59a2ec2749cd58c22c008e528372bd81f2be
Tarek-Sb <tksb06712@gmail.com>
2024-09-19 20:22:36 +0300

    bug

--- src/app/p/page.jsx
+++ src/app/p/page.jsx
@@ -6,9 +6,7 @@ export default function page() {
     <>
       <main className="flex justify-center px-[10px] bg-black">
         <section className="px-[12px] rounded-[20px] mt-[25px] w-full bg-blue-100">
-          <div className="w-[full] h-[300px] bg-slate-400">that's it</div>
           <AnimatedBeam/>
-          <div className="w-[full] h-[300px] bg-slate-400">that's it</div>
         </section>
       </main>
       
