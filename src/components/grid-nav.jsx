ce0033f2d7062117f9375e22ffbb31c899fdeab3
pepars-Rashid <peparsrashid@gmail.com>
2024-09-20 17:07:27 -0700

    hover:nav-grid

--- src/components/grid-nav.jsx
+++ src/components/grid-nav.jsx
@@ -4,11 +4,11 @@ export default function GridNav() {
   return (
     <div className="hidden lg:flex flex-col justify-center items-center gap-[10px] flex-wrap flex-grow bg-[#8B5CF6] rounded-[20px] p-[10px]">
       <div className="flex flex-grow w-full gap-[10px] h-[55%]"> {/*!!!*/}
-        <div className="flex flex-col justify-between  flex-grow w-[30%] bg-[#2B2836] rounded-[10px] p-[15px]">
+        <div className="group shadow-xl shadow-[#fcf8f835] flex flex-col justify-between  flex-grow w-[30%] transform-gpu duration-300 hover:-translate-y-4 hover:bg-[#2b2836c8] bg-[#2B2836] rounded-[10px] p-[15px]">
           <p dir="rtl" className="text-[#FFFFFF80]">
             صندوق أدواتنا 
           </p>
-          <div dir="rtl" className="flex justify-between items-center">
+          <div dir="rtl" className="transition-all duration-300 group-hover:-translate-y-10 flex justify-between items-center">
             <p  className="text-white text-[36px]">
               أدواتنا 
             </p>
@@ -17,13 +17,13 @@ export default function GridNav() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between  flex-grow w-[50%] bg-[#2B2836] rounded-[10px] p-[15px]">
+        <div className="group shadow-xl shadow-[#fcf8f835] flex flex-col justify-between  flex-grow w-[50%] transform-gpu duration-300 hover:-translate-y-4 hover:bg-[#2b2836c8] bg-[#2B2836] rounded-[10px] p-[15px]">
           <p dir="rtl" className="text-[#FFFFFF80]">
             لماذا يجب عليك اختيار <span className="text-[#F2CD88]">TP</span>
             <br/>
             لتصميم الموقع الذي يمثل شركتك
           </p>
-          <div dir="rtl" className="flex justify-between items-center">
+          <div dir="rtl" className="transition-all duration-300 group-hover:-translate-y-10 flex justify-between items-center">
             <p  className="text-white text-[36px]">
               خدماتنا 
             </p>
@@ -34,11 +34,11 @@ export default function GridNav() {
         </div>
       </div>
       <div className="flex flex-grow w-full gap-[10px] h-[30%]"> {/*!!!*/}
-        <div className="flex flex-col justify-between  flex-grow w-[50%] bg-[#2B2836] rounded-[10px] p-[15px]">
+        <div className="group shadow-xl shadow-[#fcf8f835] flex flex-col justify-between  flex-grow w-[50%] transform-gpu duration-300 hover:translate-y-4 hover:bg-[#2b2836c8] bg-[#2B2836] rounded-[10px] p-[15px]">
           <p dir="rtl" className="text-[#FFFFFF80]">
             أجوبة لكل أسئلتك
           </p>
-          <div dir="rtl" className="flex justify-between items-center">
+          <div dir="rtl" className="transition-all duration-300 group-hover:-translate-y-10 flex justify-between items-center">
             <p  className="text-white text-[36px]">
               تواصل معنا
             </p>
@@ -47,11 +47,11 @@ export default function GridNav() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between  flex-grow w-[30%] bg-[#2B2836] rounded-[10px] p-[15px]">
+        <div className="group shadow-xl shadow-[#fcf8f835] flex flex-col justify-between  flex-grow w-[30%] transform-gpu duration-300 hover:translate-y-4 hover:bg-[#2b2836c8] bg-[#2B2836] rounded-[10px] p-[15px]">
           <p dir="rtl" className="text-[#FFFFFF80]">
             ألقِ نظرة إلى أعمالنا
           </p>
-          <div dir="rtl" className="flex justify-between items-center">
+          <div dir="rtl" className="transition-all duration-300 group-hover:-translate-y-10 flex justify-between items-center">
             <p  className="text-white text-[36px]">
               أعمالنا 
             </p>
