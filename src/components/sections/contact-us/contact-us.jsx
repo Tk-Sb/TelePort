a8fd8192b26fa757a6fc69d0fcb2335020802e0c
Tarek-Sb <tksb06712@gmail.com>
2024-09-23 12:44:19 +0300

    adding form + metadata

--- src/components/sections/contact-us/contact-us.jsx
+++ src/components/sections/contact-us/contact-us.jsx
@@ -1,6 +1,7 @@
 import BoxReveal from "@/components/text-reveal";
 import GridNav from "@/components/grid-nav";
 import DockDemo from "@/components/dock/dock";
+import EmailForm from "./email-from";
 
 export default function ContactUs() {
   return (
@@ -61,28 +62,10 @@ export default function ContactUs() {
               </div>
             </div>
           </div>
-          <div className="h-[2px] md:w-[2px] md:h-auto bg-[#7E7E7E] ">
 
-          </div>
-          <div className="flex-grow ">
-            <div className="flex flex-col gap-[5px] ">
-              <label className="indent-[15px] text-[#000000] ">
-                الاسم
-              </label>
-              <input type="text" className="w-[300px] h-[50px] border border-[#7E7E7E] bg-transparent rounded-[15px] indent-[15px] " placeholder="الاسم" />
-              <label className="indent-[15px] text-[#000000] ">
-                البريد الإلكتروني
-              </label>
-              <input type="text" className="w-[300px] h-[50px] border border-[#7E7E7E] bg-transparent rounded-[15px] indent-[15px] " placeholder="you@company.com" />
-              <label className="indent-[15px] text-[#000000] ">
-                الرسالة
-              </label>
-              <textarea type="text" className="w-[300px] h-[150px] resize-none border border-[#7E7E7E] bg-transparent rounded-[15px] indent-[15px] " placeholder="اترك لنا رسالة..." />
-              <button className="md:text-[18px] w-[300px] h-[50px] flex justify-center items-center rounded-[15px] border-[2px] border-[#8B5CF6] bg-[#8B5CF6] font-medium text-white transition-all duration-200 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:border-gray-600 hover:shadow-[4px_4px_0px_#6643B6] active:translate-x-[0px] active:translate-y-[0px] active:shadow-none">
-                أرسل لنا رسالة
-              </button>
-            </div>
-          </div>
+          <div className="h-[2px] md:w-[2px] md:h-auto bg-[#7E7E7E] "></div>
+
+          <EmailForm></EmailForm>
         </div>
       </section>
     </>
